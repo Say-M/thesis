43e1e0c557db6bd23441182ae6de99ed676a03f1
Mohammad Sayem <msayem089@gmail.com>
2026-02-19 04:13:03 +0600

    chore: add @next/third-parties dependency and integrate Google Analytics in layout component

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import TanStackProvider from "@/contexts/tanstack";
@@ -113,6 +114,7 @@ export default function RootLayout({
           </AuthProvider>
         </TanStackProvider>
       </body>
+      <GoogleAnalytics gaId="G-63VP74W5V4" />
     </html>
   );
 }
